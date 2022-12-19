9d142d60c477e5dcb33e033eaa0824b06612934a
dallas <dallas.wty@gmail.com>
2022-12-19 18:22:33 +0900

    1219

---
+++ others/vue-clone/webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entry : {
+        app: './main.js'
+    },
+    module : {
+
+    },
+    plugins : [],
+    output : {
+        filename : '[name].js',
+        path : './dist'
+    }
+
+}
