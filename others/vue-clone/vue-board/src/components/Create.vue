0696085a604f6a00d16bfd01658afef8340c4d17
dallas <dallas.wty@gmail.com>
2022-12-21 18:21:34 +0900

    1221

---
+++ others/vue-clone/vue-board/src/components/Create.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <input v-model='writer' type="text" placeholder="글쓴이" />
+        <input v-model='title' type="text" placeholder="제목" />
+        <textarea v-model='content' placeholder="내용"></textarea>
+        <button @click="write">글쓰기</button>
+    </div>
+</template>
+
+<script>
+import data from '@/data'
+
+export default {
+    name : 'Create',
+    data() {
+        return {
+            data : data,
+            writer : "",
+            title : "",
+            content : ""
+        }
+    },
+    methods : {
+        write() {
+            this.data.push({
+                writer : this.writer,
+                title : this.title,
+                content : this.content
+            })
+            this.$router.push({
+                path : '/'
+            })
+        }
+    }
+}
+</script>
---
+++ others/vue-clone/vue-board/src/components/Read.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <table>
+            <tr>
+                <td>글쓴이</td>
+                <td>제목</td>
+                <td>내용</td>
+            </tr>
+            <tr v-for="(item,idx) in data" :key="idx" @click="detail(idx)">
+                <td>{{item.writer}}</td>
+                <td>{{item.title}}</td>
+                <td>{{item.content}}</td>
+            </tr>
+        </table>
+        <button @click="write">글쓰기</button>
+    </div>
+</template>
+<script>
+import data from '@/data/index.js'
+
+export default {
+    name : 'Read',
+    data() {
+        return {
+            data : data
+        }
+    },
+    methods : {
+        write() {
+            this.$router.push({
+                path: 'create'
+            })
+        },
+        detail(idx) {
+            this.$router.push({
+                name : 'Detail',
+                params : {
+                    contentId : idx
+                }
+            })
+        }
+    }
+}
+</script>
