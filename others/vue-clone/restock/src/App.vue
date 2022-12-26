ee4553b92129eaf42d3c763cfff7ee8aabd08b68
dallas <dallas.wty@gmail.com>
2022-12-26 16:24:05 +0900

    modal

--- others/vue-clone/restock/src/App.vue
+++ others/vue-clone/restock/src/App.vue
@@ -9,14 +9,15 @@
 <template>
   <div id="app">
     <header>
-      <h1>부자아빠</h1>
-      
+      <div>
+        <h1>부자아빠<i class="fa fa-question-circle" aria-hidden="true" @click="footerOn"></i></h1>
+      </div>
     </header>
     <main>
       <mobileHome v-if="mobile"/>
       <router-view/>
     </main>
-    <footer>
+    <nav>
       <!-- tablet version -->
       <div id="nav_tablet" v-if="tablet">
         <button>
@@ -36,6 +37,27 @@
 
       <!-- desktop version -->
       <div id="nav_desktop" v-if="desktop">desktop mode : {{width}}</div>
+    </nav>
+
+    <footer>
+      <div class="modal" id='modalbg' v-if="modal">
+        <div class="modal-body">
+          <div class="modal-body-title">
+            <h2>부자아빠</h2>
+            <i id='modalClose' class="fa fa-times" aria-hidden="true" @click="footerClose"></i>
+          </div>
+          <div class="modal-body-main">
+            <div class="modal-sns">
+              <i class="fa fa-facebook-official" aria-hidden="true"></i>
+              <i class="fa fa-instagram" aria-hidden="true"></i>
+              <i class="fa fa-github" aria-hidden="true"></i>
+              <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+            </div>
+            <p>대표 연락처 : dallas.wty@gmail.com</p>
+            <p><i class="fa fa-copyright" aria-hidden="true"></i>2022 All rights reserved</p>
+          </div>
+        </div>
+      </div>
     </footer>
 
   </div>
@@ -51,13 +73,7 @@ export default {
       mobile : false,
       tablet : false,
       desktop : false,
-      menus : {
-        "main" : '부자아빠',
-        'stock' : "주식관리",
-        'balance' : "재무제표",
-        'portfolio' : "포트폴리오",
-        'economy' : "주요 경제 지표"
-      }
+      modal : true
     }
   },
   components : {
@@ -91,8 +107,15 @@ export default {
             }
           }
         },
+        footerOn() {
+          this.modal = !this.modal;
+        },
+        footerClose() {
+            this.modal = !this.modal
+          }
+        }
     }
-}
+
 </script>
 
 <style>
@@ -103,48 +126,134 @@ export default {
   background-color: var(--color-dark);
   color : var(--color-white);
   height:100%;
+  max-width: 1200px;
+  min-height:650px;
+  margin:auto;
 }
 
-footer {
+header div {
+  width:100%;
+  background-color:var(--color-tomato);
+  position:relative;
+}
+header div h1 {
+  display:block;
+  text-align:center;
+  padding:8px;
+}
+header div i {
+  position:relative;
+  margin-left:1%;
+  font-size:110%;
+}
+header div i:hover{
+  font-size:130%;
+  cursor:pointer;
+}
+
+
+nav {
   display:hidden;
   width:100%;
 }
 
-footer button {
+nav button {
   width:25%;
   padding:10px;
   font-size:24px;
   background-color: var(--color-dark);
 }
 
-footer button .nav-link {
+nav button .nav-link {
   font-weight: bold;
   color: var(--color-white);
 }
 
-footer a.router-link-exact-active {
+nav a.router-link-exact-active {
   color: var(--color-tomato);
 }
 
-footer button .nav_icon {
+nav button .nav_icon {
   display: block;
   font-size:36px;
   margin-bottom:8px;
 }
 
 @media screen and (max-width:573px) {
-  footer button {
+  nav button {
     width:33%;
     padding:4px;
     font-size:var(--font-medium);
     line-height:4px;
   }
-  footer .nav-link:hover {
+  nav .nav-link:hover {
     color : var(--color-tomato)
   }
 
-  footer a.router-link-exact-active {
+  nav a.router-link-exact-active {
     color: var(--color-white)
   }
 }
+
+.modal {
+  position:absolute;
+  top:0;
+  left:0;
+  width:100vw;
+  height:100vh;
+  background-color:rgba(0, 0, 0, 0.4);
+  z-index:1;
+}
+
+.modal-body {
+  width:50%;
+  position:absolute;
+  left:25%;
+  top:20vh;
+  z-index:2;
+}
+
+.modal-body-title {
+  height:5vh;
+  background-color:var(--color-tomato);
+  position:relative;
+}
+.modal-body-title h2 {
+  font-size:1.2rem;
+  padding:12px;
+  text-align:center;
+  vertical-align:middle;
+}
+.modal-body-title i {
+  position:absolute;
+  top:5px;
+  right:10px;
+  font-size:20px;
+  cursor:pointer;
+}
+
+.modal-body-main {
+  height:15vh;
+  background-color: var(--color-white);
+  text-align:center;
+}
+.modal-body-main .modal-sns {
+  width:100%;
+  margin:auto;
+}
+.modal-body-main .modal-sns i {
+  margin:16px 0;
+  font-size:12px;
+  color:var(--color-dark);
+  width:15%;
+  font-size:24px;
+}
+.modal-body-main p {
+  color:var(--color-dark);
+  margin-bottom:5%;
+}
+
+
+
+
 </style>
